bf4017d9f5c3b5ce3430d6855f9675618de70532
Olena <alenguhm@gmail.com>
2025-11-18 13:58:16 +0200

    add styles

--- src/componenst/App/App.tsx
+++ src/componenst/App/App.tsx
@@ -1,4 +1,14 @@
+import css from "./App.module.css";
+
 function App() {
-  console.log("Hello");
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        {/* Компонент SearchBox */}
+        {/* Пагінація */}
+        {/* Кнопка створення нотатки */}
+      </header>
+    </div>
+  );
 }
 export default App;
